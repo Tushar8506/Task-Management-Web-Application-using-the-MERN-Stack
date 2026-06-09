3b6ed5c574d5c3b728487578017b253040c91888
Tushar8506 <ts.8506900@gmail.com>
2026-06-09 16:03:47 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,9 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin:['http://localhost:5173',
+            "https://task-management-client-0lzk.onrender.com"
+            ],
      credentials: true,
 }))
 
